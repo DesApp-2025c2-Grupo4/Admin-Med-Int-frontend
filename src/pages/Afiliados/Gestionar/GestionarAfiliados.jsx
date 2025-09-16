2b683ee3bfe18e9d9ac4aabd398bef14fbade1ad
brendalera <brendalera17@gmail.com>
2025-09-16 04:55:08 -0300

    agregando primer modelo de gestion de afiliados

--- src/pages/Afiliados/Gestionar/GestionarAfiliados.jsx
+++ src/pages/Afiliados/Gestionar/GestionarAfiliados.jsx
@@ -1,3 +1,30 @@
-export function GestionarAfiliados(){
-    return <h2>Gestionar Afiliados</h2>
+import "./GestionarAfiliados.css";
+import { TitleSection } from "../../../components/TitleSections/TitleSection.jsx";
+import { CardGrupo } from "./ui/CardGrupo.jsx";
+import { SearchIcon } from "./icons/SearchIcon.jsx";
+import { listAfiliados } from "./../../../Mock/listAfiliados.js";
+
+export function GestionarAfiliados() {
+  const titulares = listAfiliados?.filter((l) => l.esTitular);
+  console.log(titulares);
+  return (
+    <>
+      <TitleSection text="Gestión de Afiliados"></TitleSection>
+      <section className="section_container box-border">
+        <SearchIcon></SearchIcon>
+        <section className="section_cards">
+          {titulares.map((a) => (
+            <CardGrupo
+              key={a.personaId}
+              credencial={a.personaId}
+              nombre={a.nombre}
+              apellido={a.apellido}
+              fechaAlta={a.fechaAlta}
+              planMedico={a.planMedico.descripcion}
+            ></CardGrupo>
+          ))}
+        </section>
+      </section>
+    </>
+  );
 }
---
+++ src/pages/Afiliados/Gestionar/ui/CardGrupo.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router";
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { ModifierIcon } from "../icons/ModifierIcon";
+import { UsersIcon } from "../icons/UsersIcon";
+import "./CardGrupo.css";
+
+export function CardGrupo({
+  credencial,
+  nombre,
+  apellido,
+  fechaAlta,
+  planMedico,
+}) {
+  return (
+    <>
+      <section className="card_container box-border">
+        <section className="body-cardGrupo">
+          <h1 className="titleGrupo">
+            Grupo Familiar {credencial} | {nombre} {apellido}
+          </h1>
+          <p className="descriptionGrupo">
+            Plan: {planMedico} | Fecha Alta: {fechaAlta}
+          </p>
+          <div className="container_icons">
+            <Link>
+              <DeleteIcon></DeleteIcon>
+            </Link>
+            <Link>
+              <UsersIcon></UsersIcon>
+            </Link>
+            <Link>
+              <ModifierIcon></ModifierIcon>
+            </Link>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
