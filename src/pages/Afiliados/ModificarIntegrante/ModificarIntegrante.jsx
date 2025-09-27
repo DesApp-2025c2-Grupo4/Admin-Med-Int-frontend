49f7ee89fe421b6864f08d3d431f11cd48de93fd
rpagliarulo <rocioyasmin.pagliarulo@estudiantes.unahur.edu.ar>
2025-09-27 20:45:45 -0300

    base

--- src/pages/Afiliados/ModificarIntegrante/ModificarIntegrante.jsx
+++ src/pages/Afiliados/ModificarIntegrante/ModificarIntegrante.jsx
@@ -1,16 +1,16 @@
-import { TitleSection } from "../../../components/TitleSections/TitleSection.jsx"
-import { FormGrupoFamilia } from "../Nuevo/FormGrupoFamilia/FormGrupoFamilia.jsx"
-import { AddMember } from "../../../components/ui/AddMember/AddMember.jsx"
-import { Register } from "../../../components/ui/Register/Register.jsx"
-export function ModificarIntegrante(){
-    return (
-        <section className="section__nuevo-grupo-familiar-container box-border">
-            <TitleSection text="Modificar Integrante" />
-            <FormGrupoFamilia text="Datos del integrante" />
-            <div className="button-container">
-                <AddMember />
-                <Register />
-            </div>
-        </section>
-    )
-}
+ import { TitleSection } from "../../../components/TitleSections/TitleSection.jsx"
+ import  { FormModificarIntegrante } from "./FormModificarIntegrante/FormModificarIntegrante.jsx" 
+ import { AddMember } from "../../../components/ui/AddMember/AddMember.jsx"
+ import { Register } from "../../../components/ui/Register/Register.jsx"
+ export function ModificarIntegrante(){
+     return (
+         <section className="section__modificar-integrante-container box-border">
+             <TitleSection text="Modificar Integrante" />
+             <FormModificarIntegrante text="Datos del integrante" />
+             <div className="button-container">
+                 <AddMember />
+                 <Register />
+             </div>
+         </section>
+     )
+ }
