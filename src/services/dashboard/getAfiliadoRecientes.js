9fb49b01b8ef09165ec28626cb9e5a90b0a7b1e7
brendalera <brendalera17@gmail.com>
2025-10-16 15:21:14 -0300

    agregando tooltip en dashboard

--- src/services/dashboard/getAfiliadoRecientes.js
+++ src/services/dashboard/getAfiliadoRecientes.js
@@ -1,9 +1,7 @@
 const URL_API = import.meta.env.VITE_URL_API;
-console.log(URL_API)
 export const getAfiliadoRecientes = async () => {
   const res = await fetch(`${URL_API}/persona`)
   const data = await res.json()
-  console.log(data)
   //Despues reemplazar por la logica
   const afiliadosOrdenados = data.sort((a, b) => {
     return new Date(b.fechaAlta) - new Date(a.fechaAlta);
