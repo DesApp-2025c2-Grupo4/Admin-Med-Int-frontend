01cab5f51f4ad9e2bcdbb0eac91fb5549e96a142
brendalera <brendalera17@gmail.com>
2025-10-20 20:37:46 -0300

    agregando funcionalidad para eliminar un prestador de db

--- src/services/prestadores/eliminarUnPrestador.js
+++ src/services/prestadores/eliminarUnPrestador.js
@@ -1,6 +1,11 @@
 const URL_API = import.meta.env.VITE_URL_API;
 export const eliminarUnPrestador = async (id) => {
-  const res = await fetch(`${URL_API}/prestador/${id}`);
+  const res = await fetch(`${URL_API}/prestador/${id}`, {
+    method:'DELETE'
+  });
+
+  if (!res.ok) throw new Error('Error al eliminar el prestador');
+
   const data = await res.json();
   return new Promise((resolve) => {
     setTimeout(() => {
