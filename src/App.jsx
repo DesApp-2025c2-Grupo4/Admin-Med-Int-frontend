e1dbef51d08489eeed2fd59dcb76f2ec6a6bd5e4
Álvaro Bravo <bravoalva19@gmail.com>
2025-11-07 15:48:36 -0300

    Se agregó toastify para maenjo de alertas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
 import { Header } from './components/Header/Header.jsx'
 import { NavBarRoutes } from './routes/NavBarRoutes.jsx'
+import { ToastContainer } from 'react-toastify';
 function App() {
   return (
     <>
+      {/* Contenedor global de toasts */}
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        icon={false}
+        theme="light"
+      />
       {/* Header de todas las pantallas */}
       <Header />
 
